aecdd9b0ba76c1c092ebb093579a61d53a5151e6
Anurag Roy <anuragroy@duck.com>
2022-06-13 21:38:14 +0530

    Fix recalculation

--- main.tsx
+++ main.tsx
@@ -5,33 +5,35 @@ import seedrandom from "https://esm.sh/v86/seedrandom@3.0.5/es2022/seedrandom.mi
 
 const NUMBER_OF_POKEMON = 898;
 
-const todaysDate = new Date().toDateString();
-const rng = new seedrandom(todaysDate);
-const pokemonNumber = Math.round(rng() * NUMBER_OF_POKEMON);
+const handler = async (req: Request) => {
+  const todaysDate = new Date().toDateString();
+  const rng = new seedrandom(todaysDate);
+  const pokemonNumber = Math.round(rng() * NUMBER_OF_POKEMON);
 
-const res = await fetch(`https://pokeapi.deno.dev/pokemon/${pokemonNumber}`);
-const pokemon = await res.json();
+  const res = await fetch(`https://pokeapi.deno.dev/pokemon/${pokemonNumber}`);
+  const pokemon = await res.json();
 
-const handler = (req: Request) => html({
-  title: `POTD - Pokémon of the Day`,
-  body: (
-    <div
-      class="flex flex-col items-center justify-center w-full h-screen"
-      style={{backgroundColor: pokemon.color}}
-    >
-      <img src={pokemon.imageUrl} />
-      <h1 class="text-4xl font-bold">{pokemon.name}</h1>
-      <footer class="fixed bottom-8 w-full h-6 flex items-center justify-center gap-2 text-gray-800">
-        Powered by
-        <a
-          class="flex items-center gap-2 text-sm text-black no-underline font-semibold"
-          href="https://pokeapi.deno.dev/"
-        >
-          <img alt="Poké API" src="https://pokeapi.deno.dev/assets/logo/logo.webp" class="w-5" /> Poké API
-        </a>
-      </footer>
-    </div>
-  ),
-});
+  return html({
+    title: `POTD - Pokémon of the Day`,
+    body: (
+      <div
+        class="flex flex-col items-center justify-center w-full h-screen"
+        style={{ backgroundColor: pokemon.color }}
+      >
+        <img src={pokemon.imageUrl} height="475px" width="475px" />
+        <h1 class="text-4xl font-bold">{pokemon.name}</h1>
+        <footer class="fixed bottom-8 w-full h-6 flex items-center justify-center gap-2 text-gray-800">
+          Powered by
+          <a
+            class="flex items-center gap-2 text-sm text-black font-semibold"
+            href="https://pokeapi.deno.dev/"
+          >
+            <img alt="Poké API" src="https://pokeapi.deno.dev/assets/logo/logo.webp" class="w-5" /> Poké API
+          </a>
+        </footer>
+      </div>
+    ),
+  })
+};
 
-serve(handler);    
+await serve(handler);
